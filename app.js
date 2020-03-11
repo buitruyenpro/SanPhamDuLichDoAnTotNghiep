4398896c29ca6289424682c34f8130232e5e6388
buitruyenpro <namtruyenpro95@gmail.com>
2020-03-11 23:20:11 +0700

    ok

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'hello from the server side', app: 'Natours' });
-});
-app.post('/', (req, res) => {
-  res.send('You can post this endpoint...');
+// app.get('/', (req, res) => {
+//   res.status(200).json({ message: 'hello from the server side', app: 'Natours' });
+// });
+// app.post('/', (req, res) => {
+//   res.send('You can post this endpoint...');
+// });
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    results: tours.length,
+    status: 'success',
+    data: {
+      tours
+    }
+  });
 });
 const port = 3000;
 app.listen(port, () => {

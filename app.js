35de5ab8715e4ecb81c1c8e3946165766008584f
buitruyenpro <namtruyenpro95@gmail.com>
2020-03-12 19:59:56 +0700

    ok

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
+app.use(express.json());
 // app.get('/', (req, res) => {
 //   res.status(200).json({ message: 'hello from the server side', app: 'Natours' });
 // });
@@ -17,6 +18,20 @@ app.get('/api/v1/tours', (req, res) => {
     data: {
       tours
     }
+  });
+});
+app.post('/api/v1/tours', (req, res) => {
+  //   console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+    res.status(201).json({
+      message: 'success',
+      data: {
+        tour: newTour
+      }
+    });
   });
 });
 const port = 3000;

ec4698b6c6ff347a58ba623ec043e32897bfc33c
buitruyenpro <namtruyenpro95@gmail.com>
2020-03-12 23:27:34 +0700

    ok

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,5 +1,14 @@
 const fs = require('fs');
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`));
+exports.checkID = (req, res, next, val) => {
+  console.log(`Tour id is:${val}`);
+  const id = parseInt(val);
+  if (id > tours.length) {
+    return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
+  }
+  next();
+};
+
 exports.getAllTours = (req, res) => {
   res.status(200).json({
     results: tours.length,
@@ -14,10 +23,6 @@ exports.getAllTours = (req, res) => {
 exports.getTour = (req, res) => {
   const id = parseInt(req.params.id);
   const tour = tours.find(el => el.id === id);
-  // if (id > tours.length) {
-  if (!tour) {
-    return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
-  }
   res.status(200).json({
     status: 'success',
     data: {
@@ -27,7 +32,6 @@ exports.getTour = (req, res) => {
 };
 
 exports.createTour = (req, res) => {
-  //   console.log(req.body);
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
   tours.push(newTour);
@@ -41,20 +45,12 @@ exports.createTour = (req, res) => {
   });
 };
 exports.updateTour = (req, res) => {
-  const id = parseInt(req.params.id);
-  if (id > tours.length) {
-    return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
-  }
   res.status(200).json({
     status: 'success',
     data: { tour: '<Updated tour here...>' }
   });
 };
 exports.deleteTour = (req, res) => {
-  const id = parseInt(req.params.id);
-  if (id > tours.length) {
-    return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
-  }
   res.status(204).json({
     status: 'success',
     data: null

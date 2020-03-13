125030888a55b5a1dec0ce966335a6cba42dfa15
buitruyenpro <namtruyenpro95@gmail.com>
2020-03-13 08:50:23 +0700

    ok

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const userRouter = require('./routes/userRoutes');
 //MIDDLEWARE
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(express.static(`${__dirname}/public`));
 app.use((req, res, next) => {
   console.log('Hello from the middleware 😊😊');
   next();

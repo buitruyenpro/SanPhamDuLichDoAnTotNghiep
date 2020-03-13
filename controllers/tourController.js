602d88ec698cba71859dd4d7e40d5e5fc337c67f
buitruyenpro <namtruyenpro95@gmail.com>
2020-03-13 10:04:34 +0700

    ok

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
+
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`));
 exports.checkID = (req, res, next, val) => {
   console.log(`Tour id is:${val}`);
+  // eslint-disable-next-line radix
   const id = parseInt(val);
   if (id > tours.length) {
     return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
@@ -28,7 +30,7 @@ exports.getAllTours = (req, res) => {
 };
 
 exports.getTour = (req, res) => {
-  const id = parseInt(req.params.id);
+  const id = parseInt(req.params.id, 10);
   const tour = tours.find(el => el.id === id);
   res.status(200).json({
     status: 'success',
@@ -40,9 +42,10 @@ exports.getTour = (req, res) => {
 
 exports.createTour = (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
+  // eslint-disable-next-line node/no-unsupported-features/es-syntax
+  const newTour = { id: newId, ...req.body };
   tours.push(newTour);
-  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), () => {
     res.status(201).json({
       message: 'success',
       data: {

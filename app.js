2b9be9ea8e923b6f18e07224f18d0cd96ef00596
buitruyenpro <namtruyenpro95@gmail.com>
2020-03-13 09:24:53 +0700

    ok

--- app.js
+++ app.js
@@ -4,8 +4,12 @@ const app = express();
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 //MIDDLEWARE
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(express.json());
-app.use(morgan('dev'));
+
 app.use(express.static(`${__dirname}/public`));
 app.use((req, res, next) => {
   console.log('Hello from the middleware 😊😊');

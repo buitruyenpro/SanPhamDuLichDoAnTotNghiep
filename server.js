769f9fe1e7c1dfe790d980913c16737ada8cf0ee
buitruyenpro <namtruyenpro95@gmail.com>
2020-03-14 01:59:55 +0700

    ok

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
+const mongooes = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
-
+const DB = process.env.DATABASE_LOCAL;
+mongooes
+  .connect(DB, { useNewUrlParser: true, userCreateIndex: true, useFindAndModify: false })
+  .then(() => {
+    // console.log(con.connections);
+    console.log('DB connection successful!');
+  });
 // console.log(process.env);
 
 // START SERVER

ffe07b84c8563705161078d027e358ef72350690
buitruyenpro <namtruyenpro95@gmail.com>
2020-03-14 15:52:10 +0700

    ok

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -34,8 +34,6 @@ exports.getTour = async (req, res) => {
 };
 
 exports.createTour = async (req, res) => {
-  // const newTour = new Tour({});
-  // newTour.save();
   try {
     const newTour = await Tour.create(req.body);
     res.status(201).json({
@@ -49,11 +47,23 @@ exports.createTour = async (req, res) => {
     });
   }
 };
-exports.updateTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: { tour: '<Updated tour here...>' }
-  });
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+
+    res.status(200).json({
+      status: 'success',
+      data: { tour }
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'dữ liệu không hợp lệ được gửi!'
+    });
+  }
 };
 exports.deleteTour = (req, res) => {
   res.status(204).json({

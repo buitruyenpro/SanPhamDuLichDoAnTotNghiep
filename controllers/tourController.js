017cdc58f65abab396be4b7f23ae97aa2187da68
buitruyenpro <namtruyenpro95@gmail.com>
2020-03-14 15:19:36 +0700

    ok

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,12 +1,5 @@
 const Tour = require('./../models/tourModel');
 
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res.status(400).json({ status: 'fail', message: 'Missing name or price' });
-  }
-  next();
-};
-
 exports.getAllTours = (req, res) => {
   res.status(200).json({
     requestedAt: req.requestTime
@@ -26,11 +19,21 @@ exports.getTour = (req, res) => {
   });
 };
 
-exports.createTour = (req, res) => {
-  res.status(201).json({
-    status: 'success',
-    data: null
-  });
+exports.createTour = async (req, res) => {
+  // const newTour = new Tour({});
+  // newTour.save();
+  try {
+    const newTour = await Tour.create(req.body);
+    res.status(201).json({
+      status: 'success',
+      tour: newTour
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err
+    });
+  }
 };
 exports.updateTour = (req, res) => {
   res.status(200).json({
--- models/tourModel.js
+++ models/tourModel.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 const tourSchema = new mongoose.Schema({
-  name: { type: String, require: [true, 'Tên là bắt buộc!'], unique: true },
+  name: { type: String, required: [true, 'Tên là bắt buộc!'], unique: true },
   rating: { type: Number, default: 4.5 },
-  price: { type: Number, require: [true, 'Giá tiền là bắt buộc!'] }
+  price: { type: Number, required: [true, 'Giá tiền là bắt buộc!'] }
 });
 const Tour = mongoose.model('Tour', tourSchema);
 

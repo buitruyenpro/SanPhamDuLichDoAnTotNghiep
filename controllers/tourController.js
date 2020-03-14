2b4f45e2cf7b40ae24962276d84d35b9d366b1fd
buitruyenpro <namtruyenpro95@gmail.com>
2020-03-14 21:45:39 +0700

    ok

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -27,6 +27,16 @@ exports.getAllTours = async (req, res) => {
     } else {
       query = query.select('-__v');
     }
+    //Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+    console.log(skip);
+    query = query.skip(skip).limit(limit);
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('Trang này không tồn tại!');
+    }
 
     //EXCUTE QUERY
     const tours = await query;

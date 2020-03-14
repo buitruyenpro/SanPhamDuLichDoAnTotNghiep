54a605e9fdbf999190e50662f49f27a4d7e240fd
buitruyenpro <namtruyenpro95@gmail.com>
2020-03-14 14:14:45 +0700

    ok

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
-const mongooes = require('mongoose');
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 const DB = process.env.DATABASE_LOCAL;
-mongooes
+mongoose
   .connect(DB, { useNewUrlParser: true, userCreateIndex: true, useFindAndModify: false })
   .then(() => {
     // console.log(con.connections);
     console.log('DB connection successful!');
   });
 // console.log(process.env);
-
+const tourSchema = new mongoose.Schema({
+  name: { type: String, require: [true, 'Tên là bắt buộc!'], unique: true },
+  rating: { type: Number, default: 4.5 },
+  price: { type: Number, require: [true, 'Giá tiền là bắt buộc!'] }
+});
+const Tour = mongoose.model('Tour', tourSchema);
 // START SERVER
 const port = process.env.PORT || 3000;
 app.listen(port, () => {

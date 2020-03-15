67d56c81ad60681c5289d09a836abe8b9e6e547f
buitruyenpro <namtruyenpro95@gmail.com>
2020-03-15 18:13:56 +0700

    ok

--- server.js
+++ server.js
@@ -6,15 +6,15 @@ const app = require('./app');
 
 const DB = process.env.DATABASE_LOCAL;
 mongoose
-  .connect(DB, { useNewUrlParser: true, userCreateIndex: true, useFindAndModify: false })
-  .then(() => {
-    // console.log(con.connections);
-    console.log('DB connection successful!');
-  });
-// console.log(process.env);
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('DB connection successful!'));
 
-// START SERVER
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`app running on port ${port}...`);
+  console.log(`App running on port ${port}...`);
 });

97adcf347241afb2c7f6b307e45305a8f1b4d16a
buitruyenpro <namtruyenpro95@gmail.com>
2020-03-15 23:58:09 +0700

    ok

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -50,19 +50,16 @@ const sendErrorProd = (err, res) => {
 };
 
 module.exports = (err, req, res, next) => {
-  // console.log(err.stack);
-
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
-
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
-  } else if (process.env.NODE_ENV === 'production') {
+  } else if (process.env.NODE_ENV.trim() === 'production') {
     let error = { ...err };
 
     if (error.name === 'CastError') error = handleCastErrorDB(error);
-    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
-    if (error.name === 'ValidationError') error = handleValidationErrorDB(error);
+    // if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    // if (error.name === 'ValidationError') error = handleValidationErrorDB(error);
 
     sendErrorProd(error, res);
   }

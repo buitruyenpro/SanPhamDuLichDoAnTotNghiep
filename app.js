3574c76aa0105fed1b414ba0b233654cb7f256a1
buitruyenpro <namtruyenpro95@gmail.com>
2020-03-15 18:25:36 +0700

    ok

--- app.js
+++ app.js
@@ -12,10 +12,7 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json());
 
 app.use(express.static(`${__dirname}/public`));
-app.use((req, res, next) => {
-  console.log('Hello from the middleware 😊😊');
-  next();
-});
+
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
 
@@ -25,5 +22,12 @@ app.use((req, res, next) => {
 // ROUTE HANDLERS
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `can't find ${req.originalUrl} on this server!`
+  });
+});
 
 module.exports = app;

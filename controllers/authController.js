6c86b616f42c1186e004c47feee33ebe70495a1d
buitruyenpro <namtruyenpro95@gmail.com>
2020-03-16 20:09:05 +0700

    ok

--- controllers/authController.js
+++ controllers/authController.js
@@ -42,6 +42,27 @@ exports.login = catchAsync(async (req, res, next) => {
   res.status(200).json({ status: 'success', token });
 });
 
+exports.protect = catchAsync(async (req, res, next) => {
+  // 1) Getting token and check of it's there
+  let token;
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+
+  if (!token) {
+    return next(new AppError('You are not logged in! Please log in to get access.', 401));
+  }
+
+  // // 2) Verification token
+  // const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+  // // 3) Check if user still exists
+  // const currentUser = await User.findById(decoded.id);
+  // if (!currentUser) {
+  //   return next(new AppError('The user belonging to this token does no longer exist.', 401));
+  // }
+});
+
 // exports.protect = catchAsync(async (req, res, next) => {
 //   // 1) Getting token and check of it's there
 //   let token;

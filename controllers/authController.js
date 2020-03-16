2f8aa81cc2c29f78057a16342474deef711fec40
buitruyenpro <namtruyenpro95@gmail.com>
2020-03-17 02:27:19 +0700

    ok

--- controllers/authController.js
+++ controllers/authController.js
@@ -85,6 +85,23 @@ exports.restrictTo = (...roles) => {
   };
 };
 
+exports.forgotPassword = catchAsync(async (req, res, next) => {
+  // 1) Get user based on POSTed email
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    return next(new AppError('There is no user with email address.', 404));
+  }
+
+  // 2) Generate the random reset token
+  const resetToken = user.createPasswordResetToken();
+  await user.save({ validateBeforeSave: false });
+
+  // // 3) Send it to user's email
+  // const resetURL = `${req.protocol}://${req.get('host')}/api/v1/users/resetPassword/${resetToken}`;
+
+  // const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.\nIf you didn't forget your password, please ignore this email!`;
+});
+
 // exports.protect = catchAsync(async (req, res, next) => {
 //   // 1) Getting token and check of it's there
 //   let token;

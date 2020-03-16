cd055f6718771f4a39fc07b89aa47b277f28f2c3
buitruyenpro <namtruyenpro95@gmail.com>
2020-03-17 03:52:12 +0700

    ok

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,7 +7,7 @@ const authController = require('./../controllers/authController');
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 router.post('/forgotPassword', authController.forgotPassword);
-// router.patch('/resetPassword/:token', authController.resetPassword);
+router.patch('/resetPassword/:token', authController.resetPassword);
 router
   .route('/')
   .get(authController.protect, userController.getAllUsers)

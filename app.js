91bf25711755bd3e90703f183e02c34da208cee5
buitruyenpro <namtruyenpro95@gmail.com>
2020-03-17 21:32:56 +0700

    ok

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 const globalErrorHandler = require('./controllers/errorController');
 
 const AppError = require('./utils/appError');
@@ -12,6 +13,14 @@ console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+// Limit requests from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!'
+});
+app.use('/api', limiter);
+
 app.use(express.json());
 
 app.use(express.static(`${__dirname}/public`));

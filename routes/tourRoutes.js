53338f8a3e4533d3873225c08db1fd819eea69b4
buitruyenpro <namtruyenpro95@gmail.com>
2020-03-18 20:53:44 +0700

    ok

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const tourController = require('./../controllers/tourController');
 const authController = require('./../controllers/authController');
-const reviewController = require('./../controllers/reviewController');
+const reviewRouter = require('./../routes/reviewRoutes');
 
 const router = express.Router();
+
+router.use('/:tourId/reviews', reviewRouter);
 
 router.route('/top-5-cheap').get(tourController.aliasTopTours, tourController.getAllTours);
 router.route('/tour-stats').get(tourController.getTourStats);
@@ -22,9 +24,5 @@ router
     authController.restrictTo('admin', 'lead-guide'),
     tourController.deleteTour
   );
-
-router
-  .route('/:tourId/reviews')
-  .post(authController.protect, authController.restrictTo('user'), reviewController.createReview);
 
 module.exports = router;

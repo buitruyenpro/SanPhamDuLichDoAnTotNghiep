fb341911b4a592b950be41190c4cb8825684c43c
buitruyenpro <namtruyenpro95@gmail.com>
2020-03-19 09:12:26 +0700

    ok

--- controllers/authController.js
+++ controllers/authController.js
@@ -67,14 +67,12 @@ exports.protect = catchAsync(async (req, res, next) => {
   if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
     token = req.headers.authorization.split(' ')[1];
   }
-
   if (!token) {
     return next(new AppError('You are not logged in! Please log in to get access.', 401));
   }
 
   // 2) Verification token
   const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
-  console.log(decoded);
 
   // 3) Check if user still exists
   const currentUser = await User.findById(decoded.id);

9715ee8fe8091e513e359207c87b6af8899e6b94
buitruyenpro <namtruyenpro95@gmail.com>
2020-03-20 11:44:58 +0700

    ok

--- app.js
+++ app.js
@@ -62,7 +62,10 @@ app.use((req, res, next) => {
 
 // ROUTE HANDLERS
 app.get('/', (req, res) => {
-  res.status(200).render('base');
+  res.status(200).render('base', {
+    tour: 'Du lich Ha Long',
+    user: 'Bui Truyen'
+  });
 });
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);

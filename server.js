a7775b085dd27698556ac9e6859d94779ced6229
buitruyenpro <namtruyenpro95@gmail.com>
2020-03-23 09:41:39 +0700

    ok

--- server.js
+++ server.js
@@ -8,13 +8,15 @@ process.on('uncaughtException', err => {
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-const DB = process.env.DATABASE_LOCAL;
-mongoose.connect(DB, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true
-});
+const DB = process.env.DATABASE.replace('<password>', process.env.DATABASE_PASSWORD);
+mongoose
+  .connect(DB + '', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('DB connection successful!'));
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {});
 process.on('unhandledRejection', err => {
